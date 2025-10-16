b61a62d7043ec9db936802b2421b7114a2e622f8
ShrykeZ <9383604+ShrykeZ@users.noreply.github.com>
2025-10-16 23:22:07 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
 #!
-git clone https://${GITHUB_TOKEN}@github.com/ShrykeZ/nRaw_NETWORK.git /workspace/myproject && chmod +x /workspace/myproject/* && bash /workspace/myproject/startup.sh
+git clone https://${GITHUB_TOKEN}@github.com/ShrykeZ/nRaw_NETWORK.git /workspace/myproject && chmod +x /workspace/myproject/* && nohup bash /workspace/myproject/startup.sh > /workspace/startup.log 2>&1 &
